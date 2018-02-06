2d8c6bdba0ce47b0aa7113855a04d6a249b748db
sikanderc <sikanderchowhan@gmail.com>
2018-02-06 15:39:06 -0500

    add admin sidebar menu with mouseover logic

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -2,15 +2,36 @@ import React, { Component } from 'react'
 import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
 
 class SidebarLeftOverlay extends Component {
-  state = { visible: false }
+  state = {
+    visible: false,
+  }
 
-  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+  handleMouseMove = (e) => {
+    if (e.screenY >= 229 ) {
+      if(this.state.visible && e.screenX >= 155) {
+        this.toggleVisibilityOff()
+      } else if (!this.state.visible && e.screenX <= 20){
+        this.toggleVisibilityOn()
+      }
+    }
+  }
+
+  toggleVisibilityOn = () => {
+    if (!this.state.visible) {
+      this.setState({ visible: true })
+    }
+  }
+
+  toggleVisibilityOff = () => {
+    if(this.state.visible) {
+      this.setState({ visible: false})
+    }
+  }
 
   render() {
     const { visible } = this.state
     return (
-      <div>
-        <Button onClick={this.toggleVisibility}>Toggle Visibility</Button>
+      <div onMouseMove={this.handleMouseMove}>
         <Sidebar.Pushable as={Segment}>
           <Sidebar as={Menu} animation='overlay' width='thin' visible={visible} icon='labeled' vertical inverted>
             <Menu.Item name='home'>
